836b8a352a12f5e70f3bd10be1b7b1881e1210a4
jordannesstravel-hub <jordan.nesstravel@gmail.com>
2026-02-17 09:43:20 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body>{children}</body>
+      <body className="min-h-screen bg-white"></body>
     </html>
   );
 }
